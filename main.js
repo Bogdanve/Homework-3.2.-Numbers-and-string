8f72647e3d7f7e11caf6b8dc09eaffd861eff4d4
Bogdanve <m1ravbogd4@gmail.com>
2024-08-07 22:56:14 +0300

    2

---
+++ main.js
@@ -0,0 +1,7 @@
+let userFirstName = prompt("What's your name");
+let userFavoriteFood = prompt("What do you like from food?");
+let userFromCountry = prompt("Where are you from");
+
+alert(
+  `Name - ${userFirstName}, fav food - ${userFavoriteFood}, a country - ${userFromCountry}.`
+);
